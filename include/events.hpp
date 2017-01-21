90e1b2a250714bd8e1f370ae64627b5f939ea123
Joakim Thorén <joakimthoren93@gmail.com>
2017-01-21 14:12:37 +0100

    removed obsolete classes and structs

--- include/events.hpp
+++ include/events.hpp
@@ -1,19 +1,7 @@
 #ifndef EVENTS_HPP
 #define EVENTS_HPP
 
-struct wPress { };
-struct aPress { };
-struct sPress { };
-struct dPress { };
 struct rPress { };
-struct ePress { };
-struct qPress { };
-struct pPress { };
-struct tPress { };
-struct MouseEvent { const double x, y; };
-struct DrawGridToggle { };
-struct CursorEnterWindow { const double x, y; };
-struct CursorExitWindow { const double x, y; };
 struct OnResize { const int width, height; };
 
 #endif //EVENTS_HPP

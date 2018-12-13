36710e992275410255aa5dc045c6af8a67837ec3
Joakim Thorén <joakimthoren93@gmail.com>
2018-12-13 18:00:32 +0100

    fixed compilation error wireframe

--- include/scene.hpp
+++ include/scene.hpp
@@ -3,16 +3,17 @@
 
 #include "renderdata.hpp"
 #include <glm/mat4x4.hpp>
+#include <vector>
 
 namespace lowpoly3d {
 
 /** The scene struct contains all data which is needed for a frame to rendered and displayed
 	(previously provided by renderquerier interface) **/
-
 struct Scene {
 	std::vector<RenderData> renderDatas;
 	glm::mat4 view;
 	float sunRadians;
+	bool showWireframes;
 };
 
 }

a43b46f75fd8dba78e76627664fbfd90fb39f038
Joakim Thorén <joakimthoren93@gmail.com>
2018-12-13 18:00:02 +0100

    Rewrote perlin noise

--- include/perlin.hpp
+++ include/perlin.hpp
@@ -4,21 +4,78 @@
 #include <stddef.h>
 #include <vector>
 #include <functional>
+#include <cmath>
+#include <cassert>
 
 namespace lowpoly3d {
+
+struct Pixel {
+	std::size_t value;
+};
+
+// Representation of an zero-indexed 2D image containing arbitrary values
+template<typename value_type>
+class Image {
+private:
+	std::vector<value_type> image;
+	std::size_t resolution;
+
+public:
+	Image(std::size_t resolution) : image(resolution * resolution), resolution(resolution) { }
+
+	const value_type& get(std::size_t i, std::size_t j) const { return image(resolution * i + j); }
+	void set(std::size_t i, std::size_t j, const value_type& value) { image(resolution * i + j) = value; }
+	std::size_t getResolution() const { return resolution; }
+
+	// Stretches this image into the larger resolution, using bilinear interpolation
+	Image stretch(std::size_t largerResolution) const {
+		assert(largerResolution >= resolution);	
+		Image stretchedImage(largerResolution);
+
+		/* 	Loop over all pixels of the stretched image and find corresponding
+			values in this image using bilinear interpolation.
+			First off, find corresponding pixel in original image
+			(think of it like taking the pixel of larger image into [0.0, 1.0]
+			and then upscaling it back to [0.0, resolution]) */
+		for(std::size_t i = 0; i < largerResolution; i++) {
+			for(std::size_t j = 0; j < largerResolution; j++) {
+				// First compute a floating-point pixel-value (this can be interpreted as an interpolated pixel between integer pixel)
+				const double correspondingFloatingPointPixel = static_cast<double>(resolution) / static_cast<double>(largerResolution);
+				// Find whatever t "was used" to get correspondingPixel, i.e
+				// translate the correspondingFloatingPointPixel down to [0.0, 1.0].
+				// This number in [0.0, 1.0] is obviously the interpolation parameter.
+				const std::size_t correspondingPixel = static_cast<std::size_t>(std::floor(correspondingFloatingPointPixel));
+				const double t = correspondingFloatingPointPixel - correspondingPixel;
+				// This interpolation parameter is now used to perform bilinear interpolation
+				const value_type
+					topleft 	= image.get(correspondingPixel, correspondingPixel),
+					topright 	= image.get(correspondingPixel+1, correspondingPixel),
+					bottomleft 	= image.get(correspondingPixel, correspondingPixel+1),
+					bottomright = image.get(correspondingPixel+1, correspondingPixel+1);
+				const value_type bilerpedValue = bilerp(topleft, topright, bottomleft, bottomright, t, t);
+
+				// We have finally computed a bilerpedImage for the i,j-pixel. Only remains to set it!
+				stretchedImage.set(i, j, bilerpedValue);
+			}
+		}
+		return stretchedImage;
+	}
+};
 
 /** Generates a noise on construction in range [-1.0f, 1.0f] **/
 class Perlin {
 private:
-	size_t imageSize;
+	size_t resolution;
 	std::vector<float> image;
-	float max = 0.0f;
-	uint8_t seed = 0xB1A5EDD1CE; //Default seed
+
+	// Used to normalize noise into range [-1.0f, 1.0f]
+	float maxNoiseValue = 0.0f;
+	uint8_t seed = static_cast<uint8_t>(0xB1A5EDD1CE); //Default seed
 	void addOctave(float amplitude, float frequecy);
 public:
-	explicit Perlin(size_t imageSize, float amplitude, float frequecy, int seed);
+	explicit Perlin(size_t resolution, float amplitude, float frequecy, int seed);
 	Perlin(const Perlin& perlin);
-	explicit Perlin(size_t imageSize, size_t startOctave, size_t endOctave, int seed);
+	explicit Perlin(size_t resolution, size_t startOctave, size_t endOctave, int seed);
 
 	auto begin() { return image.begin(); }
 	auto end() { return image.end(); }
--- src/perlin.cpp
+++ src/perlin.cpp
@@ -2,69 +2,84 @@
 #include <glm/glm.hpp>
 #include <cmath>
 #include <algorithm>
-#include "misc.hpp"
+
+#include "utils/misc.hpp" // inplace_zip
+#include "utils/lerp.hpp" // lerp and bilerp
 
 namespace lowpoly3d {
 
-void Perlin::addOctave(float amplitude, float frequency) {
-	std::vector<glm::vec2> noisemap(imageSize*imageSize);
+std::vector<glm::vec2> generateRandomGradients(std::size_t resolution, std::uint8_t& seed) {
+	std::vector<glm::vec2> randomGradients(resolution*resolution);
+	auto nextr = [&seed] { return seed = (seed * 0xB105) % 233; };
 
-	auto nextr = [this] { return seed = (seed * 0xB105) % 233; };
-
-	std::generate(noisemap.begin(), noisemap.end(), [&nextr] {
+	std::generate(randomGradients.begin(), randomGradients.end(), [&nextr] {
 		return glm::vec2(nextr(), nextr());
 	});
+	return randomGradients;
+}
+
+void Perlin::addOctave(float amplitude, float frequency) {
+	std::vector<glm::vec2> noisemap = generateRandomGradients(resolution, seed);
 
 	//Loop through perlin image and compute value at each pixel
-	for(size_t i = 0; i < imageSize*imageSize; i++) {
-		const glm::vec2 p = glm::vec2(i%imageSize, i/imageSize) * ((frequency-1.0f)/imageSize)+1.0f;
+	for(size_t i = 0; i < resolution*resolution; i++) {
+		const glm::vec2 p = glm::vec2(i%resolution, i/resolution) * ((frequency-1.0f)/resolution)+1.0f;
 
-		const float c1x = floorf(p.x), c1y = floorf(p.y);
-		const float c2x = ceilf(p.x), c2y = c1y;
-		const float c3x = c1x, c3y = ceilf(p.y);
+		const float c1x = std::floor(p.x), c1y = std::floor(p.y);
+		const float c2x = std::ceil(p.x), c2y = c1y;
+		const float c3x = c1x, c3y = std::ceil(p.y);
 		const float c4x = c2x, c4y = c3y;
+
+		std::size_t 
+			i1 = c1y * resolution + c1x,
+			i2 = c2y * resolution + c2x,
+			i3 = c3y * resolution + c3x,
+			i4 = c4y * resolution + c4x;
+
+		assert(i1 < noisemap.size());
+		assert(i2 < noisemap.size());
+		assert(i3 < noisemap.size());
+		assert(i4 < noisemap.size());
+
+		// Compute interpolation parameters for bilinear interpolation
 		const float u = p.x - c1x, v = p.y - c1y;
+		image[i] += amplitude * bilerp(
+			glm::dot({u, v}, noisemap[i1]),
+			glm::dot({p.x-c2x, p.y-c2y}, noisemap[i2]),
+			glm::dot({p.x-c3x, p.y-c3y}, noisemap[i3]),
+			glm::dot({p.x-c4x, p.y-c4y}, noisemap[i4]),
+			u, v);
 
-		const auto lerp = [](float a, float b, float t) { return (1.0f - t) * a + t * b; };
-		const float 
-		x1 = lerp(
-			glm::dot({u, v}, noisemap[c1y * imageSize + c1x]),
-			glm::dot({p.x-c2x, p.y-c2y}, noisemap[c2y * imageSize + c2x]), u),
-		x2 = lerp(
-			glm::dot({p.x-c3x, p.y-c3y}, noisemap[c3y * imageSize + c3x]),
-			glm::dot({p.x-c4x, p.y-c4y}, noisemap[c4y * imageSize + c4x]), u);
-
-		image[i] += amplitude * lerp(x1, x2, v);
-		max = std::max(max, std::abs(image[i]));
+		maxNoiseValue = std::max(maxNoiseValue, std::abs(image[i]));
 	}
 }
 
-Perlin::Perlin(size_t imageSize, float amplitude, float frequency, int seed) : imageSize(imageSize), image(imageSize*imageSize), seed(seed) {
+Perlin::Perlin(size_t resolution, float amplitude, float frequency, int seed) : resolution(resolution), image(resolution*resolution), seed(seed) {
 	addOctave(amplitude, frequency);
 }
 
-Perlin::Perlin(const Perlin& perlin) : imageSize(perlin.imageSize), image(perlin.image), max(perlin.max), seed(perlin.seed) { }
+Perlin::Perlin(const Perlin& perlin) : resolution(perlin.resolution), image(perlin.image), maxNoiseValue(perlin.maxNoiseValue), seed(perlin.seed) { }
 
-Perlin::Perlin(size_t size, size_t start, size_t end, int seed) : imageSize(size), image(size*size), seed(seed) {
+Perlin::Perlin(size_t size, size_t start, size_t end, int seed) : resolution(size), image(size*size), seed(seed) {
 	for(size_t i = start; i < end; i++) {
 		addOctave(powf(2.0f, i), powf(2.0f, end-i));
 	}
 }
 
 Perlin& Perlin::binop(const Perlin& other, const std::function<float(float, float)>& binaryop) {
-	//For each binary operation made per pixel, ensure that max is updated
+	//For each binary operation made per pixel, ensure that maxNoiseValue is updated
 	inplace_zip(image, other.image, [&](float a, float b) {
 		const float binopval = binaryop(a, b);
-		max = std::max(max, std::abs(binopval));
+		maxNoiseValue = std::max(maxNoiseValue, std::abs(binopval));
 		return binopval;
 	});
 	return *this;
 }
 
 Perlin& Perlin::operator=(Perlin perlin) {
-	std::swap(imageSize, perlin.imageSize);
+	std::swap(resolution, perlin.resolution);
 	std::swap(image, perlin.image);
-	std::swap(max, perlin.max);
+	std::swap(maxNoiseValue, perlin.maxNoiseValue);
 	return *this;
 }
 
@@ -73,7 +88,7 @@ Perlin& Perlin::operator-=(const Perlin& perlin) { return binop(perlin, [](float
 Perlin& Perlin::operator*=(const Perlin& perlin) { return binop(perlin, [](float a, float b) { return a*b; }); }
 Perlin& Perlin::operator/=(const Perlin& perlin) { return binop(perlin, [](float a, float b) { return a/b; }); }
 
-float Perlin::get(size_t x, size_t y) const { return x < imageSize && y < imageSize ? image[y * imageSize + x] / max : 0.0f;}
+float Perlin::get(size_t x, size_t y) const { return x < resolution && y < resolution ? image[y * resolution + x] / maxNoiseValue : 0.0f;}
 float Perlin::operator()(size_t x, size_t y) const { return get(x, y); }
 
 Perlin operator+(Perlin a, const Perlin& b) { return a += b; }

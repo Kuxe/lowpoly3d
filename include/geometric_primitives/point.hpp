d1d7959601b9bea4fafce43523a5b1ca429a7de8
Joakim Thorén <joakimthoren93@gmail.com>
2019-01-01 16:19:49 +0100

    implemented more robust almostEqual for TPoint

--- include/geometric_primitives/point.hpp
+++ include/geometric_primitives/point.hpp
@@ -21,14 +21,20 @@ TPoint<value_type, dimension> midpoint(
 	return value_type(0.5)*(point1 + point2);
 };
 
-// Returns true if ||p1-p2|| <= eps
+// Returns true if p1 and p2 are componentwise almost equal with ulp=2
 template<typename value_type, std::size_t dimension>
 bool almostEqual(
 	const TPoint<value_type, dimension>& p1,
 	const TPoint<value_type, dimension>& p2,
-	value_type eps = std::numeric_limits<value_type>::epsilon()) {
+	int ulp = 2) {
 
-	return glm::distance(p1, p2) <= eps;
+	// Source: https://en.cppreference.com/w/cpp/types/numeric_limits/epsilon
+	auto almostEqual = [&ulp](auto x, auto y) {
+		auto const ulp = 2;
+		return std::abs(x-y) <= std::numeric_limits<decltype(x)>::epsilon() * std::abs(x+y) * ulp
+			|| std::abs(x-y) < std::numeric_limits<decltype(x)>::min();
+	};
+	return almostEqual(p1.x, p2.x) && almostEqual(p1.y, p2.y) && almostEqual(p1.z, p2.z);
 }
 
 using Pointf = TPoint<float, 3>;

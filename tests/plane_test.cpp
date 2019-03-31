c2ee68f4f0851dd0efa7116e3455bfd4c7caf6d6
Joakim Thorén <joakimthoren93@gmail.com>
2019-03-31 11:56:24 +0200

    Added tests to 'Projecting points onto local plane'

--- tests/plane_test.cpp
+++ tests/plane_test.cpp
@@ -159,4 +159,54 @@ SCENARIO("Projecting points onto local plane") {
 			}
 		}
 	}
+
+	GIVEN("A Plane((2,1,0),(-1/sqrt(2),-1/sqrt(2),0)) and a three points p1, p2, p3, with p2 being exactly above (2,1,0) in the direction of the plane-normal and the line segment (p1,p3) is perpendicular to the plane normal") {
+		auto const point = Point(2.0f, 1.0f, 0.0f);
+		float const cathetus = -1.0f/sqrtf(2.0f);
+		auto const normal = Point(cathetus, cathetus, 0.0f);
+		auto const plane = Plane(point, normal);
+		Point const
+			p1 = {0.0f,0.0f,0.0f},
+			p2 = {1.0f,0.0f,0.0f},
+			p3 = {1.0f,1.0f,0.0f};
+
+		WHEN("Projecting all three points locally into the plane") {
+			auto const
+				localp1 = plane.projectIntoLocal(p1),
+				localp2 = plane.projectIntoLocal(p2),
+				localp3 = plane.projectIntoLocal(p3);
+
+			THEN("The point p2 is projected into zeropoint") {
+				REQUIRE(glm::all(glm::epsilonEqual(localp1, Point2f(0.0f, 0.0f), eps)));
+			}
+
+			THEN("The points p1, p3 are projected into the same point") {
+				INFO("localp1=" << glm::to_string(localp1));
+				INFO("localp2=" << glm::to_string(localp3));
+				REQUIRE(glm::all(glm::epsilonEqual(localp1, localp3, eps)));
+			}
+		}
+	}
+
+	GIVEN("A plane((1,0,0),(1,0,0)) and three points p1=(0,0,0), p2=(1,0,0), p3=(1,1,0)") {
+		auto const point = Point(1.0f, 0.0f, 0.0f);
+		auto const normal = Point(1.0f, 0.0f, 0.0f);
+		auto const plane = Plane(point, normal);
+		Point const
+			p1 = {0.0f,0.0f,0.0f},
+			p2 = {1.0f,0.0f,0.0f},
+			p3 = {1.0f,1.0f,0.0f};
+
+		WHEN("Projecting all three points locally into the plane") {
+			auto const
+				localp1 = plane.projectIntoLocal(p1),
+				localp2 = plane.projectIntoLocal(p2),
+				localp3 = plane.projectIntoLocal(p3);
+
+			THEN("The points p1 and p2 are projected into zeropoint") {
+				REQUIRE(glm::all(glm::epsilonEqual(localp1, Point2f(0.0f, 0.0f), eps)));
+				REQUIRE(glm::all(glm::epsilonEqual(localp2, Point2f(0.0f, 0.0f), eps)));
+			}
+		}
+	}
 }

64877c2f14914c4c05b274db13848d9004784d60
Joakim Thorén <joakimthoren93@gmail.com>
2020-12-01 23:38:27 +0100

    Added RenderDataBuilder

--- include/renderdata.hpp
+++ include/renderdata.hpp
@@ -16,6 +16,20 @@ struct RenderData {
 	RenderData(const glm::mat4& modelMatrix, const std::string& model, const std::string& shader = "Default");
 };
 
+class RenderDataBuilder final
+{
+public:
+	RenderDataBuilder();
+
+	RenderData build() const;
+
+	RenderDataBuilder& setTransformationInWorld(glm::mat4 const& iTransformationInWorld);
+	RenderDataBuilder& setModel(std::string const& iModel);
+	RenderDataBuilder& setShader(std::string const& iShader);
+private:
+	RenderData mRenderData;
+};
+
 }
 
 #endif //RENDERDATA_HPP
--- src/renderdata.cpp
+++ src/renderdata.cpp
@@ -5,4 +5,30 @@ namespace lowpoly3d {
 RenderData::RenderData(const glm::mat4& modelMatrix, const std::string& model, const std::string& shader) :
 	modelMatrix(modelMatrix), model(model), shader(shader) { }
 
+
+RenderDataBuilder::RenderDataBuilder() = default;
+
+RenderData RenderDataBuilder::build() const
+{
+	return mRenderData;
 }
+
+RenderDataBuilder& RenderDataBuilder::setTransformationInWorld(glm::mat4 const& iTransformationInWorld)
+{
+	mRenderData.modelMatrix = iTransformationInWorld;
+	return *this;
+}
+
+RenderDataBuilder& RenderDataBuilder::setModel(std::string const& iModel)
+{
+	mRenderData.model = iModel;
+	return *this;
+}
+
+RenderDataBuilder& RenderDataBuilder::setShader(std::string const& iShader)
+{
+	mRenderData.shader = iShader;
+	return *this;
+}
+
+} // End of namespace lowpoly3d

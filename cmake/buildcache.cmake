ff3e9589c20b6eb2d51af284f87e92ce0623bb70
Joakim Thorén <joakimthoren93@gmail.com>
2022-02-06 21:54:35 +0100

    Added helpful documentation on enabling buildcache with msys2/mingw64-setup

--- cmake/buildcache.cmake
+++ cmake/buildcache.cmake
@@ -5,6 +5,12 @@
 # Might be a reason to use only buildcache anyway to keep
 # the build-configuration simple
 
+# CMake finds it's compiler in a specific order:
+# cc, gcc, cl, bcc, xlc, clang
+# See https://cmake.org/pipermail/cmake/2013-March/053819.html.
+# This is PROBLEMATIC! buildcache does not understand cc or c++.
+# Solution: Set CC and CXX environment-variables to point to gcc and g++.
+
 if(WIN32)
 	set(buildcache_archive "${CMAKE_SOURCE_DIR}/download/buildcache-windows.zip")
 	set(buildcache_bindir "${CMAKE_SOURCE_DIR}/buildcache/bin")

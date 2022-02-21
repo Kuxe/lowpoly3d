b3504d37f758c79a84472486fc184975fb405812
Joakim Thorén <joakimthoren93@gmail.com>
2022-02-21 17:30:04 +0100

    Better formatting

--- include/utils/arithmetic_invariant.hpp
+++ include/utils/arithmetic_invariant.hpp
@@ -10,17 +10,16 @@ namespace lowpoly3d {
 static constexpr bool ENABLE_THROW_ON_INVARIANT_BROKEN = true;
 
 struct not_invariant_exception final : std::runtime_error {
-    not_invariant_exception() : std::runtime_error("Invariant violation") { }
+	not_invariant_exception() : std::runtime_error("Invariant violation") { }
 };
 
 template<typename arithmetic_type>
 struct in_unit_interval {
-    static_assert(std::is_arithmetic<arithmetic_type>::value,
-        "arithmetic_type must satisfy std::is_arithmetic");
+	static_assert(std::is_arithmetic<arithmetic_type>::value, "arithmetic_type must satisfy std::is_arithmetic");
 
-    static bool check(arithmetic_type number) {
-        return 0 <= number && number <= 1;
-    }
+	static bool check(arithmetic_type number) {
+		return 0 <= number && number <= 1;
+	}
 };
 
 /* arithmetic_invariant is a wrapper around an arithmetic type
@@ -34,172 +33,210 @@ struct in_unit_interval {
  * or assertions that checks some condition */
 template<typename arithmetic_type, typename invariant_unary_checker>
 struct arithmetic_invariant : invariant_unary_checker {
-    static_assert(std::is_arithmetic<arithmetic_type>::value,
-        "arithmetic_type must satisfy std::is_arithmetic");
+	static_assert(std::is_arithmetic<arithmetic_type>::value,
+		"arithmetic_type must satisfy std::is_arithmetic");
 
-    arithmetic_type number;
+	arithmetic_type number;
 
-    arithmetic_invariant(arithmetic_type number) : number(number) {
-        if(ENABLE_THROW_ON_INVARIANT_BROKEN) {
-            if(!invariant_unary_checker::check(number)) throw not_invariant_exception();
-        }
-    }
+	arithmetic_invariant(arithmetic_type number) : number(number) {
+		if(ENABLE_THROW_ON_INVARIANT_BROKEN) {
+			if(!invariant_unary_checker::check(number)) throw not_invariant_exception();
+		}
+	}
 
-    arithmetic_invariant(const arithmetic_invariant<arithmetic_type, invariant_unary_checker>& number)
-        : arithmetic_invariant<arithmetic_type, invariant_unary_checker>(number.number) { }
+	arithmetic_invariant(const arithmetic_invariant<arithmetic_type, invariant_unary_checker>& number)
+		: arithmetic_invariant<arithmetic_type, invariant_unary_checker>(number.number) { }
 
-    template<typename other_arithmetic_type>
-    arithmetic_invariant operator=(other_arithmetic_type other) {
-        static_assert(std::is_arithmetic<other_arithmetic_type>::value,
-        "Can only assign arithmetic types to arithmetic_invariants");
-        if(ENABLE_THROW_ON_INVARIANT_BROKEN) {
-            if(!invariant_unary_checker::check(other)) throw not_invariant_exception();
-        }
-        std::swap(number, other.number);
-        return *this;
-    }
-
-    arithmetic_type operator-() const {
-        return -number;
-    }
-
-    /**************************/
-    /** Comparison operators **/
-    /**************************/
 	template<typename other_arithmetic_type>
-    bool operator<(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number < other.number;
-    }
+	arithmetic_invariant operator=(other_arithmetic_type other) {
+		static_assert(
+			std::is_arithmetic<other_arithmetic_type>::value,
+			"Can only assign arithmetic types to arithmetic_invariants"
+		);
+		if(ENABLE_THROW_ON_INVARIANT_BROKEN) {
+			if(!invariant_unary_checker::check(other)) throw not_invariant_exception();
+			}
+			std::swap(number, other.number);
+			return *this;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator<=(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number <= other.number;
-    }
+	arithmetic_type operator-() const {
+		return -number;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator>(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number > other.number;
-    }
+	/**************************/
+	/** Comparison operators **/
+	/**************************/
+	template<typename other_arithmetic_type>
+	bool operator<(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number < other.number;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator>=(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number >= other.number;
-    }
+	template<typename other_arithmetic_type>
+	bool operator<=(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number <= other.number;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator==(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number == other.number;
-    }
+	template<typename other_arithmetic_type>
+	bool operator>(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number > other.number;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator!=(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
-        return number != other.number;
-    }
+	template<typename other_arithmetic_type>
+	bool operator>=(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number >= other.number;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator<(other_arithmetic_type other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number < other;
-    }
+	template<typename other_arithmetic_type>
+	bool operator==(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number == other.number;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator<=(other_arithmetic_type other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number <= other;
-    }
+	template<typename other_arithmetic_type>
+	bool operator!=(arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) const {
+		return number != other.number;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator>(other_arithmetic_type other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number > other;
-    }
+	template<typename other_arithmetic_type>
+	bool operator<(other_arithmetic_type other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number < other;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator>=(other_arithmetic_type other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number >= other;
-    }
+	template<typename other_arithmetic_type>
+	bool operator<=(other_arithmetic_type other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number <= other;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator==(other_arithmetic_type other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number == other;
-    }
+	template<typename other_arithmetic_type>
+	bool operator>(other_arithmetic_type other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number > other;
+	}
 
-    template<typename other_arithmetic_type>
-    bool operator!=(other_arithmetic_type other) const {
-		static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
-        return number != other;
-    }
+	template<typename other_arithmetic_type>
+	bool operator>=(other_arithmetic_type other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number >= other;
+	}
+
+	template<typename other_arithmetic_type>
+	bool operator==(other_arithmetic_type other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number == other;
+	}
+
+	template<typename other_arithmetic_type>
+	bool operator!=(other_arithmetic_type other) const {
+	static_assert(std::is_arithmetic<other_arithmetic_type>::value, "other_arithmetic_type must be arithmetic");
+		return number != other;
+	}
 };
 
 // Addition
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator+(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) {
+auto operator+(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other)
+{
 	return number.number + other.number;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator+(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const other_arithmetic_type other) {
-    return number.number + other;
+auto operator+(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const other_arithmetic_type other)
+{
+	return number.number + other;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator+(const other_arithmetic_type other, const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number) {
-    return number.number + other;
+auto operator+(
+	const other_arithmetic_type other,
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number)
+{
+	return number.number + other;
 }
 
 // Subtraction
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator-(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) {
+auto operator-(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other)
+{
 	return number.number - other.number;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator-(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const other_arithmetic_type other) {
-    return number.number - other;
+auto operator-(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const other_arithmetic_type other)
+{
+	return number.number - other;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator-(const other_arithmetic_type other, const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number) {
-    return other - number.number;
+auto operator-(
+	const other_arithmetic_type other,
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number)
+{
+	return other - number.number;
 }
 
 // Multiplication
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator*(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) {
+auto operator*(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other)
+{
 	return number.number * other.number;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator*(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const other_arithmetic_type other) {
-    return number.number * other;
+auto operator*(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const other_arithmetic_type other)
+{
+	return number.number * other;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator*(const other_arithmetic_type other, const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number) {
-    return number.number * other;
+auto operator*(
+	const other_arithmetic_type other,
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number)
+{
+	return number.number * other;
 }
 
 // Division
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator/(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other) {
+auto operator/(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const arithmetic_invariant<other_arithmetic_type, invariant_unary_checker> other)
+{
 	return arithmetic_type(1) / number.number * other;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator/(const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number, const other_arithmetic_type other) {
-    return number.number / other;
+auto operator/(
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number,
+	const other_arithmetic_type other)
+{
+	return number.number / other;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
-auto operator/(const other_arithmetic_type other, const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number) {
-    return number.number / other;
+auto operator/(
+	const other_arithmetic_type other,
+	const arithmetic_invariant<arithmetic_type, invariant_unary_checker> number)
+{
+	return number.number / other;
 }
 
 template<typename T, typename U, typename V>
@@ -225,29 +262,31 @@ bool operator>=(U other, T number) {
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
 bool operator==(
 	other_arithmetic_type other,
-	arithmetic_invariant<arithmetic_type, invariant_unary_checker> number) {
+	arithmetic_invariant<arithmetic_type, invariant_unary_checker> number)
+{
 	return number == other;
 }
 
 template<typename arithmetic_type, typename other_arithmetic_type, typename invariant_unary_checker>
 bool operator!=(
 	other_arithmetic_type other,
-	arithmetic_invariant<arithmetic_type, invariant_unary_checker> number) {
+	arithmetic_invariant<arithmetic_type, invariant_unary_checker> number)
+{
 	return number != other;
 }
 
 template<typename arithmetic_type>
 struct true_invariant {
-    static bool check(arithmetic_type) {
-        return true;
-    }
+	static bool check(arithmetic_type) {
+		return true;
+	}
 };
 
 template<typename arithmetic_type>
 struct false_invariant {
-    static bool check(arithmetic_type) {
-        return false;
-    }
+	static bool check(arithmetic_type) {
+		return false;
+	}
 };
 
 template<typename arithmetic_type>
@@ -260,8 +299,8 @@ using arithmetic_false_invariant = arithmetic_invariant<arithmetic_type, false_i
  * said guarantee by if-checks */
 template<typename floating_point_type>
 using LerpParameter = arithmetic_invariant<
-    floating_point_type,
-    in_unit_interval<floating_point_type>>;
+	floating_point_type,
+	in_unit_interval<floating_point_type>>;
 
 } // End of namespace lowpoly3d
 
